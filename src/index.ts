281446c8deb0f77154cbe5022b6ff803bf454f68
Calvin Colton <calvincolton@gmail.com>
2020-12-18 11:31:02 -0800

    'User' class with fetch and save functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello web framework')
+import { User } from './models/User';
+
+// const user = new User({ id: 1 });
+
+// user.set({ name: 'Michael Jackson', age: 0 });
+
+// user.save();
+
+const user = new User({ name: 'Billy Jean', age: 16 });
+user.save();
