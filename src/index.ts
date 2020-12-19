6ff0fe090cb2753aae65f36fedec37131562f82d
Calvin Colton <calvincolton@gmail.com>
2020-12-18 18:16:48 -0800

    extract user functionality to separate classes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { User } from './models/User';
 
 // const user = new User({ id: 1 });
+// user.events.on('change', () => {
+//   console.log('change!');
+// });
 
-// user.set({ name: 'Michael Jackson', age: 0 });
-
-// user.save();
-
-const user = new User({ name: 'Billy Jean', age: 16 });
-user.save();
+// user.events.trigger('change');
