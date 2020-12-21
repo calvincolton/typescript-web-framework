68723d79361efbad880445ca17d2b42f6aa6ce1a
Calvin Colton <calvincolton@gmail.com>
2020-12-21 11:32:39 -0800

    buildUserCollection extracted to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -19,8 +20,15 @@ export class User extends Model<UserProps> {
       new ApiSync<UserProps>(rootUrl)
     )
   }
-
-  isAdminUser(): boolean {
-    return this.get('id') === 1;
+  
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(
+      rootUrl,
+      (json: UserProps) => User.buildUser(json)
+    );
   }
+
+  // isAdminUser(): boolean {
+  //   return this.get('id') === 1;
+  // }
 };
