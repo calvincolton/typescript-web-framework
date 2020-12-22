9107348cf07acb98f814b95f36df2a8bb790369d
Calvin Colton <calvincolton@gmail.com>
2020-12-21 19:24:48 -0800

    re-render and clear parent element inner HTML

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,29 +1,35 @@
 import { User } from '../models/User';
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover
+      'click:.set-age': this.onSetAgeClick
     };
   }
 
-  onButtonClick(): void {
-    console.log('hi there');
-  }
-
-  onHeaderHover(): void {
-    console.log('h1 was hovered over')
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
   }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me</button>
+        <button class="set-age">Set Random Age</button>
       </div>
     `;
   }
@@ -40,6 +46,7 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     
