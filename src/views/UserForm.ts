60a9fe63af4916e6a253a7a9f2b8bcaa4a5d7089
Calvin Colton <calvincolton@gmail.com>
2020-12-21 19:11:03 -0800

    event handling on UserForm

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,5 +1,22 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('hi there');
+  }
+
+  onHeaderHover(): void {
+    console.log('h1 was hovered over')
+  }
 
   template(): string {
     return `
@@ -11,9 +28,23 @@ export class UserForm {
     `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      
+      fragment.querySelectorAll(selector).forEach(elem => {
+        elem.addEventListener(eventName, eventsMap[eventKey]);
+      })
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    
+    this.bindEvents(templateElement.content);
+
     this.parent.append(templateElement.content);
   }
 }
